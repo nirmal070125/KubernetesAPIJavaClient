bb0d965438bd8864e6b5d18d84164c190beae301
Carlos Sanchez <carlos@apache.org>
2015-01-26 14:08:20 +0100

    Make AbstractKubernetesModelList iterable

--- src/main/java/com/github/kubernetes/java/client/model/AbstractKubernetesModelList.java
+++ src/main/java/com/github/kubernetes/java/client/model/AbstractKubernetesModelList.java
@@ -1,11 +1,12 @@
 package com.github.kubernetes.java.client.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
-public abstract class AbstractKubernetesModelList<T> extends AbstractKubernetesModel {
+public abstract class AbstractKubernetesModelList<T> extends AbstractKubernetesModel implements Iterable<T> {
 
     private List<T> items = new ArrayList<T>();
 
@@ -19,6 +20,10 @@ public abstract class AbstractKubernetesModelList<T> extends AbstractKubernetesM
 
     public void setItems(List<T> items) {
         this.items = items;
+    }
+
+    public Iterator<T> iterator() {
+        return getItems().iterator();
     }
 
     @Override
